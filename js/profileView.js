ec6f0bf5f28f0195740ea41b9cf4bceb4bebaf58
SGGregory76 <sggregory76@gmail.com>
2025-04-28 06:34:50 -0700

    Create profileView.js

---
+++ js/profileView.js
@@ -0,0 +1,10 @@
+[
+  {
+    "id":"blaze",
+    "name":"Blaze",
+    "moodRules":[ /* e.g. heat >20 → grumpy */ ],
+    "inventory":[{"item":"weed","qty":2},…],
+    "loyaltyRules":[ … ]
+  },
+  …
+]
