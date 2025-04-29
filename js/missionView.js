af730a6d0485ca9109af8294c5480d02015d6d4f
SGGregory76 <sggregory76@gmail.com>
2025-04-28 20:52:01 -0700

    Delete js/missionView.js

--- js/missionView.js
+++
@@ -1,6 +0,0 @@
-GameState.updateStats(mission.rewards);
-if (mission.id==='prologue') {
-  localStorage.setItem('prologueDone','true');
-}
-GameState.addMission(mission.id);
-location.hash = '#/' + mission.next;
