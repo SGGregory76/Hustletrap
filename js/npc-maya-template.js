99ad203c9bdbe31a3f4140e194b2f23ace0f2149
SGGregory76 <sggregory76@gmail.com>
2025-05-04 19:40:13 -0700

    Create npc-maya-template.js

---
+++ js/npc-maya-template.js
@@ -0,0 +1,19 @@
+{
+  "id": "maya",
+  "name": "Maya",
+  "emoji": "💊",
+  "specialty": "xanax",
+  "currentDrug": "xanax",
+  "role": "supplier",
+  "mood": "withdrawn",
+  "loyalty": 65,
+  "trust": 40,
+  "heat": 5,
+  "inventory": [
+    { "item": "xanax", "type": "drug", "qty": 2 },
+    { "item": "ziploc", "type": "resource", "qty": 5 }
+  ],
+  "storageLimit": 5,
+  "lastUsed": "2025-05-04T12:00:00Z",
+  "nextAutoAction": "2025-05-05T04:00:00Z"
+}
